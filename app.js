ea602bb650f960e0a1a25b756ad16ae4b6954863
abdullapro2006 <135731252+abdullapro2006@users.noreply.github.com>
2023-09-07 18:53:52 +0400

    Add files via upload
    Backgorund color changed but l cant solved percentage function:(

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const buttons = document.querySelectorAll("input");
+
+const buttons = document.querySelector(".percantage");
+const display = document.querySelector(".display")
 
 let output = "";
 
-const calculate = (Value) =>{
-  if(Value ==="=" && Value !== ""){
-   output = eval(output.replace("%","/100"))
+const calculate = (Value)=>{
+  if(Value === "=" && Value !== "" ){
+    output = eval(output.replace("%","/100"));
+
+
   }
 
-
-  
+  display.value = output;
 }
+
+
+buttons.forEach((button)=>{
+  button.addEventListener("click",(e) => calculate(e.target.dataset.value));
+})
+ 
+
